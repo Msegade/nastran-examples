cf391c6ebf6275ea77d9a966f3e702b4ce478137
Miguel Segade <miguelrsegade@gmail.com>
2022-06-22 23:22:26 +0200

    refactor timeload.py

--- dynamics/1dof/timeLoad.py
+++ dynamics/1dof/timeLoad.py
@@ -1,24 +1,26 @@
 from pyNastran.bdf.bdf import BDF
 import numpy as np
+import subprocess
 
 bdf = BDF()
-bdf.read_bdf('1dof-damped.bdf')
+bdf.read_bdf('1dof-load.bdf')
 
-sub = bdf.subcases[0]
-sub.add('DLOAD', 111, [], 'STRESS-type')
-
-bdf.add_darea(10, 2, 2, 1.0)
-bdf.add_tload1(111, 10, 444, 0.0, 0)
 tstep = bdf.tsteps[888]
 dt = tstep.DT[0]
 n = tstep.N[0]
-
 
 w = 3.4 # rad/s
 P = 0.7 # load amplitude
 x = np.linspace(0.0, dt*n, n)
 y = P*np.sin(w*x)
 
-bdf.add_tabled1(444, x, y)
+tbdf = BDF()
+tbdf.punch = True
+tbdf.add_tabled1(444, x, y)
+tbdf.add_darea(10, 2, 2, 1.0)
+tbdf.add_tload1(111, 10, 444, 0.0, 0)
+tbdf.write_bdf('tload.bdf')
 
-bdf.write_bdf('1dof-load.bdf')
+# Add a blank line to the end to avoid nastran errors
+with open('tload.bdf', 'a') as outfile:
+    subprocess.run(['echo', '""'], shell=True, stdout=outfile)
